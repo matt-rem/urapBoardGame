8f5d6379c4d9aa59d5a4b81b471aa37005abd43a
kashvi-kashew <55931457+kashvi-kashew@users.noreply.github.com>
2021-02-23 01:10:48 -0800

    Update player.py

--- player.py
+++ player.py
@@ -4,4 +4,8 @@ class Player:
 
 	def __init__(self, string):
 		self.player_name = string
+		#make sure none of the players at the beginning of game are winners
+		#self.is_winner = false
+		
+		
 	

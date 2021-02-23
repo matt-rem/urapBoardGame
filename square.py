1bda534f5c73ec3e6092e2e179868e7a92314ef0
kashvi-kashew <55931457+kashvi-kashew@users.noreply.github.com>
2021-02-23 01:07:46 -0800

    Update square.py

--- square.py
+++ square.py
@@ -19,3 +19,14 @@ class Square:
 
 	def getPlayers(self):
 		return players
+	
+	'''wanted to add methods for snakes and ladders if the square is either the bottom of a ladder or the start of a snake, we can set
+	the position of the square the player will eventually land on. 
+	
+	def set_successor(self, s):
+		self.successor.x = s.x
+		self.successor.y = s.y
+	
+	'''
+	
+	

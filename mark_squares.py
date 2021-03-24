2b8561e135f115b8c0355b0009e77f7392ad2475
Richard Privalov <riprivalov@berkeley.edu>
2021-03-23 19:33:52 -0700

    s

--- mark_squares.py
+++ mark_squares.py
@@ -7,6 +7,7 @@ import sys
 
 #square position number
 sq_num = 1
+undoCount = 0
 
 #first time opens a csv file (the csv file must already exist)
 '''
@@ -28,6 +29,9 @@ dataArray.append([name, playercount,'', '', '', '', '', '', ''])
 def click_event(event, x, y, flags, params): 
     global sq_num;
       # checking for left mouse clicks 
+    global undoCount;
+    if event != 0:
+        print(event, "going in")
     if event == cv2.EVENT_LBUTTONDOWN: 
   
         # displaying the coordinates 
@@ -44,28 +48,24 @@ def click_event(event, x, y, flags, params):
         '''
         dataArray.append(['','', x, y, sq_num, sq_num + 1,'','', ''])
 
-        #on the image show the square position number's x, y coordinate
-        cv2.putText(img,str(sq_num), (x,y), font, 
-                    0.5, (0, 0, 0), 2) 
+        if undoCount == 0:
+            #on the image show the square position number's x, y coordinate
+            cv2.putText(img,str(sq_num), (x,y), font, 
+                        0.5, (0, 0, 0), 2) 
+        else:
+            #on the image show the square position number's x, y coordinate
+            print("UNDO")
+            cv2.putText(img,str(sq_num), (x,y), font, 
+                        0.5, (255, 0, 0), 2)   
+            undoCount -= 1
         cv2.imshow('image', img)
+
 
         #increment the square position for the next click
         sq_num += 1
-    # checking for right mouse clicks: Will prompt to enter 2 square numbers and that will be a ladder.
-    if event == cv2.EVENT_RBUTTONDOWN: 
+    
   
-        # displaying the coordinates 
-        # on the image window 
-        #print(dataArray)
-        first = input("Enter number of the first square of the ladder or slide: ")
-        second = input("Enter number of the second square of the ladder or slide: ")
         
-        for i in range(2, len(dataArray)):
-            #print(dataArray[i][4], first)
-            if int(dataArray[i][4]) == int(first):
-                dataArray[i][6] = "ladder"
-                dataArray[i][7] = second
-                break
     
   
 # driver function 
@@ -91,21 +91,57 @@ if __name__=="__main__":
   
     # press any key to exit 
     #cv2.waitKey(0)
-    cv2.waitKey(0) 
+    running = True
+    while running:
+        k = cv2.waitKey(0);
+        print(k)
+        #Press escape for exit from program
+        if k == 27:
+            print(dataArray)
+            #Delete old csv file
+            f = open("sample.csv", "w")
+            f.truncate()
+            f.close()
 
 
-    print(dataArray)
-    #Delete old csv file
-    f = open("sample.csv", "w")
-    f.truncate()
-    f.close()
-    #Write into csv file
-    with open('sample.csv', mode='a') as sfile:
-        f_writer = csv.writer(sfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        for i in dataArray:
-            f_writer.writerow(i)
-    
-  
-    # close the window 
-    cv2.destroyAllWindows() 
+            #Final edits: Sets square 1 as start, square n as dice, and square n - 1 as finish
+            for i in range(2, len(dataArray)):
+                #print(dataArray[i][4], first)
+                if int(dataArray[i][4]) == 1:
+                    dataArray[i][6] = "start"
+                if int(dataArray[i][4]) == len(dataArray) - 3:
+                    dataArray[i][6] = "finish"
+                if int(dataArray[i][4]) == len(dataArray) - 2:
+                    dataArray[i][6] = "dice"
+
+            #Write into csv file
+            with open('sample.csv', mode='a') as sfile:
+                f_writer = csv.writer(sfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                for i in dataArray:
+                    f_writer.writerow(i)
+            
+          
+            # close the window 
+            cv2.destroyAllWindows() 
+            running = False
+        # Press L for ladder
+        if k == 108: 
+            # displaying the coordinates 
+            # on the image window 
+            #print(dataArray)
+            first = input("Enter number of the first square of the ladder or slide: ")
+            second = input("Enter number of the second square of the ladder or slide: ")
+            
+            for i in range(2, len(dataArray)):
+                #print(dataArray[i][4], first)
+                if int(dataArray[i][4]) == int(first):
+                    dataArray[i][6] = "ladder"
+                    dataArray[i][7] = second
+                    break
+        #Press backspace for undo. Can only undo if you numbered a square. Cannot undo ladders.
+        if k == 127: 
+            if len(dataArray) > 2:
+                undoCount += 1
+                sq_num -= 1
+                dataArray.pop()
 

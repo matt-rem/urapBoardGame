e6e290e6f89b4b8456295c39cddbd0e3bfc68d73
Cyrush <cyrushung8222@gmail.com>
2021-10-16 01:24:43 -0700

    Updated Interface, finished board upload functionality

--- application.py
+++ application.py
@@ -1,12 +1,40 @@
+import os
+import sqlite3
 from flask import Flask
-from flask import render_template
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'assets/board_images'
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SECRET_KEY'] = "secret"
 
 @app.route("/")
 def home():
     return render_template('home.html')
 
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file:
+            db = sqlite3.connect('database.db')
+            name = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], name))
+            db.execute('''INSERT INTO boards (file_name) VALUES (?)''', (name,)) #current error: unique parameter failed (should return error html instead)
+            db.commit()
+            db.close()
+            return redirect("/")
+
 if __name__ == '__main__':
-    app.debug = True
     app.run()
